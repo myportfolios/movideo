d0dec6039a46980d0adcae2022fac16070262aea
myportfolios <lexybiade@gmail.com>
2020-03-22 23:58:42 -0400

    test2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Test</h1>
+        <h2>Test2</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
