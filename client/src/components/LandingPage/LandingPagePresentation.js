456b04a1230bd34efef859b39e4e532cd168bd31
myportfolios <lexybiade@gmail.com>
2020-04-21 23:21:24 -0400

    added landing page text

--- client/src/components/LandingPage/LandingPagePresentation.js
+++ client/src/components/LandingPage/LandingPagePresentation.js
@@ -4,25 +4,50 @@ import { BASE_URL, POSTER_SIZES } from "services/api";
 
 import "./landing-page.scss";
 
-export default function LandingPagePresentation({ imagesList }) {
+export default function LandingPagePresentation({ imagesList, viewport }) {
+  // let POSTER_SIZES = getPosterSizeFromViewport(viewport);
   let imagesListJSX = imagesList?.map(item => {
-    console.log("item", item);
     // console.log(`${BASE_URL}${POSTER_SIZES}${item.images}`);
     let imageSrc = `${BASE_URL}${POSTER_SIZES}${item.images}`;
     // console.log("imageSrc", imageSrc);
     // console.log("imageSrc", typeof imageSrc);
     return (
       // <Card>
-      <img src={imageSrc} alt="image" key={item.images} />
+
+      <img
+        src={imageSrc}
+        alt="image"
+        key={item.images}
+        style={{ opacity: "0.1" }}
+      />
+
       // </Card>
     );
   });
 
   return (
     <div className="landing-presentation">
-      {/* <h1>Landing Page Presentation Component</h1> */}
-      {/* <Card imagesList={imagesList} />; */}
       {imagesListJSX}
+      {renderWelcomeBox()}
     </div>
   );
 }
+
+// const getPosterSizeFromViewport = viewport => {
+//   let posterSize = Math.floor(viewport.viewportwidth / 2);
+//   console.log(posterSize);
+//   return posterSize;
+// };
+
+export const renderWelcomeBox = () => {
+  return (
+    <div className="render-welcome-box">
+      <h2>Welcome Text</h2>
+      <h5>Register or Sign In to create collection of movies</h5>
+      <div>
+        <button>Register</button>
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
+};
