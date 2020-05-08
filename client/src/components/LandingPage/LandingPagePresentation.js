b088652c61c27cff600b350fdc73e50fe423a9a9
myportfolios <lexybiade@gmail.com>
2020-05-08 00:32:10 -0400

    commit b4 double register form

--- client/src/components/LandingPage/LandingPagePresentation.js
+++ client/src/components/LandingPage/LandingPagePresentation.js
@@ -9,6 +9,7 @@ export default function LandingPagePresentation({ imagesList }) {
     <div>
       <ImagesTable imagesList={imagesList} />
       {renderWelcomeBox()}
+      <RegistrationLoginComponent />
     </div>
   );
 }
@@ -16,16 +17,47 @@ export default function LandingPagePresentation({ imagesList }) {
 export const renderWelcomeBox = () => {
   return (
     <div className="render-welcome-box">
-      <h2>Welcome Text</h2>
-      <h5>Register or Sign In to create collection of movies</h5>
+      <h2 className="welcome-header">Movideo</h2>
+      <h5 className="welcome-para">
+        Register or Sign In to create collection of movies
+      </h5>
       <MenuBtnComponent>
         {({ register, signIn }) => (
-          <div>
-            <button>{register}</button>
-            <button>{signIn}</button>
+          <div className="welcome-btns">
+            <button className="btn btn--register">{register}</button>
+            <button className="btn btn--signIn">{signIn}</button>
           </div>
         )}
       </MenuBtnComponent>
+    </div>
+  );
+};
+
+export const RegistrationLoginComponent = () => {
+  return (
+    <div className="registrationLoginDiv">
+      <div className=" inputContainer">
+        <div className="inputContainer--fullname">
+          <h5 className="text">Fullname</h5>
+          <input type="text" name="fullname" placeholder="fullname" />
+        </div>
+        <div className="inputContainer--email">
+          <h5 className="text">Email</h5>
+          <input type="text" name="email" placeholder="email" />
+        </div>
+        <div className="inputContainer--password">
+          <h5 className="text">Password</h5>
+          <input type="text" name="password" placeholder="password" />
+        </div>
+        <div className="inputContainer--confirmPassword">
+          <h5 className="text">Confirm Password</h5>
+          <input
+            type="text"
+            name="confirmPassword"
+            placeholder="confirmPassword"
+          />
+        </div>
+      </div>
     </div>
   );
 };
